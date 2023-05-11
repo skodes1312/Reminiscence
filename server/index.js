a3641d16f0c6108be0de05a270a0891d3f14bfd2
skodes1312 <kannaujiyasunny1312@gmail.com>
2023-05-11 21:57:33 +0530

    Add routes controllers and model for posts

--- server/index.js
+++ server/index.js
@@ -3,18 +3,29 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+//Cnnecting to Mongodb Database
+
+const CONNECTION_URL =
+  "mongodb+srv://aryachawdhary:Reminiscence2023@cluster0.bzuf1ws.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database Connected.");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
+
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.status(200).send("Server Setup");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
